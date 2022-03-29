65005013c073cf210e5b9de782b034d2a9a23e84
M7md-Yousry <mymysm711@gmail.com>
2022-03-29 15:31:38 +0200

    Adding Project

---
+++ app/src/main/java/com/example/slws/myapplication/About.java
@@ -0,0 +1,11 @@
+package com.example.slws.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.slws.myapplication.R.layout.about);
+    }
+}
---
+++ app/src/main/java/com/example/slws/myapplication/splash.java
@@ -0,0 +1,32 @@
+package com.example.slws.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.slws.myapplication.MainActivity;
+import com.example.slws.myapplication.R;
+
+
+
+public class splash extends Activity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(splash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
